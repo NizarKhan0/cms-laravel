5ed2d64a1998a3eb4eb4f3f6e95d46c6d85e35d4
Nizar Khan <nizarkhan7071@gmail.com>
2026-04-10 03:43:45 +0800

    backend user

---
+++ resources/views/layouts/backend-user/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.backend-user')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <div class="col-span-12">
+        <h1 class="text-2xl font-bold text-gray-800 dark:text-white">Admin Dashboard</h1>
+        <!-- Content dashboard anda di sini -->
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ Route::get('/', function () {
 });
 
 
-Route::get('/admin', function () {
-    return view('layouts.backend-user');
-});
+//backend user
+
+//redirect after login
+Route::get('/admin/dashboard', function () {
+    return view('layouts.backend-user.dashboard');
+})->name('adminDashboard');
+
+// manage backend user
+//list
+Route::get('/admin/list-backend-user', function () {
+    return view('layouts.backend-user.module.user.index');
+})->name('backendUserList');
+//create
+//update
+//delete
